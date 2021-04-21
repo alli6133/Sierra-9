415d5d73e99f98e83f5bd83635ebd5106cfd4def
Agzzel <sterneraxel@protonmail.com>
2021-04-21 17:43:21 +0200

    uppdaterat laserkanonen.
    Lasermissiler skjuts ut genom att trycka på en UI-button. Knappen är en prefab med tillhörande script.

--- New Unity Project/Assets/Scripts/LaserCannon.cs
+++ New Unity Project/Assets/Scripts/LaserCannon.cs
@@ -1,38 +1,38 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class LaserCannon : MonoBehaviour
 {
     public Rigidbody2D missile;
+    public Button button;
+    public GameObject player;
+    public Vector3 playerPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);//�ndra siffran vid problem
-            if (touch.phase == TouchPhase.Began)
-            {
-                if (Input.GetKeyDown(KeyCode.Q) == true)
-                {
-                    Rigidbody2D clone;
-                    clone = Instantiate(missile, transform.position, transform.rotation);
-                    clone.velocity = new Vector2(13.0f, 0.0f);
-                }
-            }
-        }
-
+        player = GameObject.Find("Player");
+        playerPos = player.transform.position;
     }
 
-    /*Ett laserskott skjuts samtidigt som Q trycks ner. 
-     * m�ste minska eldhastigheten
-     * 
+    public void FireMissile()
+    {
+        Rigidbody2D clone;
+        clone = Instantiate(missile, playerPos, transform.rotation);
+        clone.velocity = new Vector2(13.0f, 0.0f);
+    }
+
+    /*Ett laserskott skjuts samtidigt som en UI-button trycks. 
+     * Tidigare så sköt spelaren genom att trycka på nedre vänstra hörnet
+     * med Input.touch.
+     * Nu har jag ändrat så man trycker på en UI-button.
      * */
 }
