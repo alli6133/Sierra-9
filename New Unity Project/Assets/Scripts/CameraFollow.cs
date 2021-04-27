d2b8d0894e61b638bdea1989220f490a62fe0a15
Agzzel <sterneraxel@protonmail.com>
2021-04-27 14:03:47 +0200

    ändrat cameramovement

--- New Unity Project/Assets/Scripts/CameraFollow.cs
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -10,19 +10,14 @@ public class CameraFollow : MonoBehaviour
     GameObject followTarget;
 
     private Vector3 offset = new Vector3(12f, 0f, -10);
-    public float smoothSpeed = 0.1f;
     void Start()
     {
-        followTarget = GameObject.Find("Player"); //leta upp player och gör det till followTargets värde
+        //followTarget = GameObject.Find("Player"); //leta upp player och gör det till followTargets värde
     }
 
 
     void LateUpdate()//anropas när alla update-funktioner har exekverats
     {
-        Vector3 desiredPosition = followTarget.transform.position + offset;//kameran hamnar på spelarens position, Z -10.
-        Vector3 smoothPosition = Vector3.Lerp(gameObject.transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        //spelarens aktuella värde och de önskade värdet adderas det räknas ut på en viss tid.
-        //detta för att kameran ska flytta på sig långsamt
-        gameObject.transform.position = smoothPosition;
+        
     }
 }
