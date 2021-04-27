1ebbc227e5285622439f52d649d674bdb8aa987d
Agzzel <sterneraxel@protonmail.com>
2021-04-27 12:35:35 +0200

    Ändrat storlek på sprites + kamera

--- New Unity Project/Assets/Scripts/EnemyLaser.cs
+++ New Unity Project/Assets/Scripts/EnemyLaser.cs
@@ -7,7 +7,7 @@ public class EnemyLaser : MonoBehaviour
 {
     public Rigidbody2D missile;
     public GameObject enemyLauncher;
-    public Vector3 launcherPos;
+    public Quaternion launcherRot;//prova ändra till rotation istället
     public Rigidbody2D clone;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip firesound;
@@ -22,13 +22,13 @@ public class EnemyLaser : MonoBehaviour
     void Update()
     {
         enemyLauncher = GameObject.Find("enemyLauncher");
-        launcherPos = enemyLauncher.transform.position;
+        launcherRot = enemyLauncher.transform.rotation;
         FireMissile();
     }
 
     public void FireMissile()
     {
-        clone = Instantiate(missile, launcherPos, missile.transform.rotation);
+        clone = Instantiate(missile, missile.transform.position, missile.transform.rotation);
         clone.velocity = new Vector2(13.0f, 0.0f);
         audioSource.PlayOneShot(firesound);
     }
