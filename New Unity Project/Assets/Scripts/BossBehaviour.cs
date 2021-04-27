d87c16181190230fc0ae850fec5fb85d770504ee
Agzzel <sterneraxel@protonmail.com>
2021-04-27 15:58:28 +0200

    Bossen kan skjuta poggers

--- New Unity Project/Assets/Scripts/BossBehaviour.cs
+++ New Unity Project/Assets/Scripts/BossBehaviour.cs
@@ -9,20 +9,45 @@ public class BossBehaviour : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private float speed = 5f; 
     [SerializeField] private float frequency = 5f; 
-    [SerializeField] private float radius = 5f; 
+    [SerializeField] private float radius = 5f;
 
-    //[SerializeField] private float offset = 0f; 
+    public Rigidbody2D missile;
+    public GameObject bossLauncher;
+    public Vector3 launcherPos;
+    public Rigidbody2D clone;
+
+    [SerializeField] private float laserTimer;
+    [SerializeField] private float laserCooldown = 0.7f;
 
     void Start()
     {
         rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         myPosition = gameObject.transform.position;
+        laserTimer = laserCooldown;
     }
 
     void Update()
     {
-        transform.position = myPosition + transform.up * Mathf.Sin(Time.time * frequency /*+ offset*/) * radius + transform.right * Time.time * speed;
+        transform.position = myPosition + transform.up * Mathf.Sin(Time.time * frequency) * radius + transform.right * Time.time * speed;
+        bossLauncher = GameObject.Find("bossLauncher");
+        launcherPos = bossLauncher.transform.position;
+
+        if (laserTimer < 0)
+        {
+            FireMissile();
+        }
+        else
+        {
+            laserTimer -= Time.deltaTime;
+        }
+    }
+
+    public void FireMissile()
+    {
+        clone = Instantiate(missile, launcherPos, missile.transform.rotation);
+        clone.velocity = new Vector2(-13.0f, 0.0f);
+        laserTimer = laserCooldown;
     }
 
 }
