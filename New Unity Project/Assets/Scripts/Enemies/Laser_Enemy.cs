e77935ab417c4eb7d56f81512d2e62e4e55061e1
Agzzel <sterneraxel@protonmail.com>
2021-05-01 14:10:09 +0200

    lagt till fiender i level1

--- New Unity Project/Assets/Scripts/Enemies/Laser_Enemy.cs
+++ New Unity Project/Assets/Scripts/Enemies/Laser_Enemy.cs
@@ -5,20 +5,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehaviour : MonoBehaviour
+public class Laser_Enemy : MonoBehaviour
 {
     private PlayerState ps;
     private Vector3 myPosition;
     //private Rigidbody2D rigidBody2D; 
     //private SpriteRenderer spriteRenderer;
 
-    public int maxHealth = 1;
+    public int maxHealth = 2;
     public int attackDamage = 1;
     public double currentHealth;
     
     [SerializeField] private float frequency = 5f;
     [SerializeField] private float radius = 5f;
     [SerializeField] private float speed = 4f;
+
+    public Rigidbody2D enemyMissile;
+    private GameObject enemyLauncher;
+    public Vector3 launcherPos;
+    public Rigidbody2D clone;
+
+    [SerializeField] private float laserTimer;
+    [SerializeField] private float laserCooldown = 0.7f;
 
     //[SerializeField] private float offset = 0f; 
 
@@ -29,6 +37,7 @@ public class EnemyBehaviour : MonoBehaviour
         //rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
         //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         myPosition = gameObject.transform.position;
+        laserTimer = laserCooldown;
     }
 
 
@@ -36,8 +45,25 @@ public class EnemyBehaviour : MonoBehaviour
     {
         //Använder sinusformeln för att röra fienden upp och ner
         transform.position = myPosition + transform.up * Mathf.Sin(Time.time * frequency /*+ offset*/) * radius + transform.right * Time.time * speed;
+        enemyLauncher = transform.Find("enemyLauncher").gameObject;
+        launcherPos = enemyLauncher.transform.position;
+
+        if (laserTimer < 0)
+        {
+            FireMissile();
+        }
+        else
+        {
+            laserTimer -= Time.deltaTime;
+        }
     }
 
+    public void FireMissile()
+    {
+        clone = Instantiate(enemyMissile, launcherPos, enemyMissile.transform.rotation);
+        clone.velocity = new Vector2(-13.0f, 0.0f);
+        laserTimer = laserCooldown;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
