8355a627b5310e3b4dd9cf1f758ae1d64292b00b
alli6133 <alexander_lindberg@yahoo.se>
2021-05-19 16:22:27 +0200

    bug fix

--- New Unity Project/Assets/Scripts/FadeOutFromBlack.cs
+++ New Unity Project/Assets/Scripts/FadeOutFromBlack.cs
@@ -18,7 +18,7 @@ public class FadeOutFromBlack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        blackOutSquare.SetActive(true);
     }
 
     // Update is called once per frame
