6bf981ad81045045aa8eab94a26681bb51b93188
alli6133 <alexander_lindberg@yahoo.se>
2021-05-20 15:03:58 +0200

    Startat attempt med att få en fade-in på start av varje level

--- New Unity Project/Assets/Scripts/AmbienceController.cs
+++ New Unity Project/Assets/Scripts/AmbienceController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AmbienceController : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
 
     [SerializeField] private AudioClip ambienceClip;
+    private bool isFaded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +21,43 @@ public class AmbienceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().name != "MainMenu" && isFaded == false)
+        {
+            FadeInAudio();
+            print("reached if-statement");
+        }
     }
 
     public void FadeOutAudio()
     {
-        StartCoroutine(StartFade(audioSource, 2, 0));
+        StartCoroutine(StartFadeOut(audioSource, 2, 0));
     }
 
-    public IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    private void FadeInAudio()
+    {
+        StartFadeIn(audioSource, 2, 0, audioSource.volume);
+        isFaded = true;
+        print("reached fadeInAudio()");
+    }
+
+    public IEnumerator StartFadeOut(AudioSource audioSource, float duration, float targetVolume)
     {
         float currentTime = 0;
         float start = audioSource.volume;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
+    }
+
+    public IEnumerator StartFadeIn(AudioSource audioSource, float duration, float startVolume, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = startVolume;
 
         while (currentTime < duration)
         {
