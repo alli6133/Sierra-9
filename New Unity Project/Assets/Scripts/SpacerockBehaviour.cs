ada6a9aec87c0cc4bff02d57b6cd36a850631d27
Agzzel <sterneraxel@protonmail.com>
2021-05-26 17:22:29 +0200

    Fixat rymdstenar

--- New Unity Project/Assets/Scripts/SpacerockBehaviour.cs
+++ New Unity Project/Assets/Scripts/SpacerockBehaviour.cs
@@ -9,7 +9,12 @@ public class SpacerockBehaviour : MonoBehaviour
     public double currentHealth;
     private PlayerState ps;
     private Vector3 rockPosition;
+    ParticleSystem system;
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float timeBeforeDeletion = 1f;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private bool removeGameObject = false;
+    private float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +22,7 @@ public class SpacerockBehaviour : MonoBehaviour
         ps = GameObject.Find("Player").GetComponent<PlayerState>();
         currentHealth = maxHealth;
         rockPosition = gameObject.transform.position;
+        system = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -24,6 +30,14 @@ public class SpacerockBehaviour : MonoBehaviour
     {
         transform.position = rockPosition + transform.right * Time.time * speed;
         //transform.Rotate(0, 0, 50 * Time.deltaTime); //Se till att göra så stenen snurrar runt medans den rör sig på ett visst håll, juicing stage
+        if (removeGameObject == true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBeforeDeletion)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,8 +55,16 @@ public class SpacerockBehaviour : MonoBehaviour
 
         if (currentHealth <= 0)
         {
-            Destroy(gameObject);
+            RockDestruction();
         }
+    }
+
+    private void RockDestruction()
+    {
+        spriteRenderer.sprite = null;
+        GetComponent<Collider2D>().enabled = false;
+        system.Play();
+        removeGameObject = true;
     }
 
 }
