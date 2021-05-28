7cb7ede8141e713b49d3b13ed6f37bf4759e28cd
alli6133 <alexander_lindberg@yahoo.se>
2021-05-28 11:10:59 +0200

    Adderat fade-out + soundeffect till restart knappen i Game Over scenen

--- New Unity Project/Assets/Scripts/Restart.cs
+++ New Unity Project/Assets/Scripts/Restart.cs
@@ -3,11 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Restart : MonoBehaviour
 {
+
+    public AudioSource audioSource;
+    public AudioClip restartPressedClip;
+    public GameObject blackOutSquare;
+    public GameObject menuButton;
+    public Image restartImage;
+    public Text restartText;
+    private bool restartPressed = false;
+    private float timer = 0f;
+    private float timeBeforeNewScene = 3f;
+
     public void Reset()
     {
-        SceneManager.LoadScene("Level1");
+        restartPressed = true;
+        menuButton.SetActive(false);
+        restartImage.enabled = false;
+        restartText.enabled = false;
+        audioSource.PlayOneShot(restartPressedClip);
+    }
+
+    void Update()
+    {
+        if (restartPressed == true)
+        {
+            StartCoroutine(FadeBlackOutSquare());
+
+            timer += Time.deltaTime;
+            if (timer >= timeBeforeNewScene)
+            {
+                SceneManager.LoadScene("Level1");
+            }
+        }
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 1)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
     }
 }
